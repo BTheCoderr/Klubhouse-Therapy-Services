ac431be31432787028e12739f0fe0ce702ff238e
Baheem Ferrell <bferrell514@gmail.com>
2025-07-19 17:43:13 -0400

    ADD: Disclaimer about required contact info and file upload

--- app/intake/page.tsx
+++ app/intake/page.tsx
@@ -22,6 +22,22 @@ export default function IntakePage() {
             <div className="w-24 h-1 bg-gradient-to-r from-yellow-400 to-orange-500 mx-auto rounded-full"></div>
           </div>
 
+          {/* Disclaimer */}
+          <div className="bg-yellow-50 border-l-4 border-yellow-400 p-4 mb-8 rounded-lg">
+            <div className="flex items-start">
+              <div className="flex-shrink-0 mt-1">
+                <svg className="h-5 w-5 text-yellow-400" fill="currentColor" viewBox="0 0 20 20">
+                  <path fillRule="evenodd" d="M8.257 3.099c.765-1.36 2.722-1.36 3.486 0l5.58 9.92c.75 1.334-.213 2.98-1.742 2.98H4.42c-1.53 0-2.493-1.646-1.743-2.98l5.58-9.92zM11 13a1 1 0 11-2 0 1 1 0 012 0zm-1-8a1 1 0 00-1 1v3a1 1 0 002 0V6a1 1 0 00-1-1z" clipRule="evenodd" />
+                </svg>
+              </div>
+              <div className="ml-3">
+                <p className="text-yellow-800">
+                  <strong>Required Information:</strong> Please ensure you provide your contact email, phone number, and attach the client's psychological evaluation to complete this form.
+                </p>
+              </div>
+            </div>
+          </div>
+
           <form 
             name="intake" 
             method="POST" 
